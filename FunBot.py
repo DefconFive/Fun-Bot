ae6ddd0cc1b5c6bc6f3bfc670c9df72b3e9ee8c1
zach78954 <zach.3000@hotmail.com>
2016-11-06 20:59:46 -0800

    Part of the Rewrite
    Part 2 of many

--- FunBot.py
+++ FunBot.py
@@ -1,4 +1,12 @@
 import discord
+import random
+import asyncio
+import sys
+import http
+import aiohttp
+import os
+import json
+
 client = discord.Client()
 #bot setup for dual testing
 #Uncomment depending on who you are.
@@ -18,7 +26,7 @@ async def on_message(message):
 	# we do not want the bot to reply to itself
 	if message.author == client.user:
 		return
-
+	#Ping command. 
 	if message.content.startswith('{}ping'.format(cmd)):
 		if message.author.id == '129437909131591680':
 			msg = 'Bang, Bang! {0.author.mention}'.format(message)
@@ -29,7 +37,12 @@ async def on_message(message):
 		else:
 			msg = 'PONG! {0.author.mention}'.format(message)
 			await client.send_message(message.channel, msg)
-
+	# cat command
+	if message.content.startswith('{}cat'.format(cmd)):
+		async with aiohttp.get('http://random.cat/meow') as r:
+			if r.status == 200:
+				js = await r.json()
+				await client.send_message(message.channel,js['file'])
 @client.event
 async def on_ready():
 	print('Logged in as')
