733b8cb5fd5376e33dfa32441ed03a538d325f56
Blake <Deathsroke790@gmail.com>
2016-11-18 00:57:23 -0600

    Setup help command

--- FunBot.py
+++ FunBot.py
@@ -14,13 +14,13 @@ client = discord.Client()
 #bot setup for dual testing
 #Uncomment depending on who you are.
 #Blake's Setup
-#Token = 'MjIzMjQ5NTQzNTc4MjU1MzYw.Cu2Dpg.1US_ESdFGSVWMLbnWJIHQd1QnQU'
+Token = 'MjIzMjQ5NTQzNTc4MjU1MzYw.Cu2Dpg.1US_ESdFGSVWMLbnWJIHQd1QnQU'
 # this is the bot command prefix
-#cmd = '-'
+cmd = '-'
 #Zach's Setup
-Token = 'MjI0NzE5NjI1OTIzODU0MzM2.Cremqw.tKWTMcD9SqF_nThAjJ3krj9uXWw'
+#Token = 'MjI0NzE5NjI1OTIzODU0MzM2.Cremqw.tKWTMcD9SqF_nThAjJ3krj9uXWw'
 # this is the bot command prefix
-cmd = '--'
+#cmd = '--'
 
 
 @client.event
@@ -30,7 +30,18 @@ async def on_message(message):
 		return
 	#help command. 
 	if message.content.startswith('{}help'.format(cmd)):
-		await client.send_message(message.Channel,'help?')
+		msg = '```Heres a list of my commands:'\
+			'\n\n-Ping - Bot simply replies ping (unless your special) useful for knowing whether or not It\'s online'\
+			'\n\n-cat - Bot posts a random cat picture'\
+			'\n\n-trump - Bot posts a random Trump gif'\
+			'\n\n-hillary - Bot posts a random Hillary gif'\
+			'\n\n-bernie - Bot posts a random Bernie gif'\
+			'\n\n-horse - Bot posts a random horse gif'\
+			'\n\n-dog - Bot posts a random dog gif'\
+			'\n\n-restart - restarts the bot (can only works for the bot owner)'\
+			'\n\n-guess - starts the guessing game```'\
+
+		await client.send_message(message.channel, msg)
 	#Ping command. 
 	elif message.content.startswith('{}ping'.format(cmd)):
 		if message.author.id == '129437909131591680':
@@ -76,9 +87,12 @@ async def on_message(message):
 		await client.send_message(message.channel,img)
 	#restart command
 	elif message.content.startswith('{}restart'.format(cmd)):
-		msg = '```Restarting!!```'
-		await client.send_message(message.channel, msg)
-		os.execl(sys.executable, sys.executable, *sys.argv)
+		if message.author.id == '183790956754108416':
+			msg = '```Restarting!!```'
+			await client.send_message(message.channel, msg)
+			os.execl(sys.executable, sys.executable, *sys.argv)
+		else:
+			msg = '```ERROR: You are not cool enough to use this command!```'
 	#guess command
 	elif message.content.startswith('{}guess'.format(cmd)):
 		await client.send_message(message.channel, 'Guess a number between 1 to 10')
